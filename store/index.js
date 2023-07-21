f743fb141600d48f6f8984ea2bd1b9ad700f2c2a
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-21 22:13:12 +0300

    now users can track the data fetching progress

--- store/index.js
+++ store/index.js
@@ -5,37 +5,45 @@ export const store = createStore({
     return {
       products: [],
       categories: [],
-      searchResults: [],
       users: [],
+      isFetching: false,
     };
   },
 
   mutations: {
     async getAllCategories(state) {
+      state.isFetching = true;
       state.categories = await $fetch('/api/getAllCategories').catch(
         (error) => error
       );
+      state.isFetching = false;
     },
 
     async getAllProducts(state) {
+      state.isFetching = true;
       const { products } = await $fetch('/api/getAllProducts').catch(
         (error) => error
       );
       state.products = products;
+      state.isFetching = false;
     },
 
     async getProductsOfCategory(state, category) {
+      state.isFetching = true;
       const { products } = await $fetch(`/api/${category}/getProducts`).catch(
         (error) => error
       );
       state.products = products;
+      state.isFetching = false;
     },
 
     async searchProducts(state, searchQuery) {
+      state.isFetching = true;
       const { products } = await $fetch(
         `/api/searchProducts?query=${searchQuery}`
       ).catch((error) => error);
-      state.searchResults = products;
+      state.products = products;
+      state.isFetching = false;
     },
 
     async getUsers(state) {
