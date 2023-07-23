6d66a1bc203412c57aaf77edebdbb2b028a72aae
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-23 21:04:24 +0300

    registered a new capitalize function for string prototype chain

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,10 @@
   const store = useStore();
   store.commit('getAllProducts');
   store.commit('getAllCategories');
+
+  String.prototype.capitalize = function () {
+    return this.at(0).toUpperCase() + this.slice(1);
+  };
 </script>
 
 <template>
