9cb9a909e8399902baf1410fbb802acf5d393720
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-24 23:15:56 +0300

    now pulling data inside the page that needs it, instead of every route change

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,4 @@
 <script setup>
-  const store = useStore();
-  store.commit('getAllProducts');
-  store.commit('getAllCategories');
-
   String.prototype.capitalize = function () {
     return this.at(0).toUpperCase() + this.slice(1);
   };
@@ -13,5 +9,6 @@
     <!-- <loadingIndicator /> -->
     <Header />
     <slot />
+    <Footer />
   </div>
 </template>
