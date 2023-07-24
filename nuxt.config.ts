2dd5a64ca66f0720015449ba98e37d2fa187d05e
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-24 23:14:51 +0300

    deleted a library that is no longer used

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'nuxt-rating'],
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
   build: {
     transpile: ['vue-toastification'],
   },
