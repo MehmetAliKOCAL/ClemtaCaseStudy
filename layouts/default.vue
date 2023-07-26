83b39dfa348ace80706493127b89cabcd05efd6c
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-26 22:04:51 +0300

    now keeping up the store even after hard page refreshes by reinitializing the states stored on the localStorage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,11 @@
 <script setup>
+  const store = useStore();
+  const loggedInUser = () => JSON.parse(localStorage.getItem('loggedInUser'));
+  onMounted(() => {
+    if (loggedInUser() !== null) {
+      store.commit('updateLoggedInUserState', loggedInUser());
+    }
+  });
   String.prototype.capitalize = function () {
     return this.at(0).toUpperCase() + this.slice(1);
   };
