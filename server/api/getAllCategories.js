a5cad5e393e71fb0cc640554fdfb138fa55f9a60
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-26 21:59:31 +0300

    removed unused code

--- server/api/getAllCategories.js
+++ server/api/getAllCategories.js
@@ -2,9 +2,7 @@ const config = useRuntimeConfig();
 
 export default defineEventHandler(async () => {
   async function getAllCategories() {
-    return await $fetch(`${config.API_URL}/categories`, {
-      method: 'GET',
-    }).catch((error) => {
+    return await $fetch(`${config.API_URL}/categories`).catch((error) => {
       return error;
     });
   }
